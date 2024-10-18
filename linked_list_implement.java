170af11f3999918eea799637aaac848fa1b77d4c
Suryansh Chaturvedi <sksuryansh00chaturvedi@gmail.com>
2024-10-18 15:25:27 +0530

    Add files via upload

---
+++ linked_list_implement.java
@@ -0,0 +1,16 @@
+public class linked_list_implement{
+    static class linkedListNode {
+        int data;
+        linkedListNode add;
+
+        //constructor
+        linkedListNode(int ch){
+            this.data = ch;
+            this.add = null;
+        }
+    }
+    public static void main(String[] args) {
+        linkedListNode ll = new linkedListNode(34);
+        System.out.println(ll.data);
+    }
+}
